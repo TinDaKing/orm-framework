e5e2dafd625ce9c08e12ec4a30e8dfd0f943577f
tindaking <82101177+TinDaKing@users.noreply.github.com>
2023-12-21 21:30:26 +0700

    init Spring project

---
+++ src/main/java/com/example/designpattern/ormframework/OrmFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.designpattern.ormframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/designpattern/ormframework/OrmFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designpattern.ormframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

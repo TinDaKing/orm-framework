7d14866796346be5384c30170a13beccaa258468
tindaking <82101177+TinDaKing@users.noreply.github.com>
2024-01-02 16:14:57 +0700

    ConnectionManager class for connect and close

---
+++ src/main/java/com/example/designpattern/ormframework/connector/ConnectionManager.java
@@ -0,0 +1,30 @@
+package com.example.designpattern.ormframework.connector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    private static Connection connection;
+
+    public static Connection getConnection(String url, String username, String password) {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
